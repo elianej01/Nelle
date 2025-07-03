7bc1e6246f155c8d98c35bbf4d2ace67f1500eba
elianej01 <elianejimenez2001@gmail.com>
2025-07-03 15:06:39 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,134 @@
+var jade=0;
+
+//The setup function only happens once
+function setup() {
+	createCanvas(500, 500); //create a 500px X 500px canvas
+   background(0,0,0);
+}
+
+//The draw function happens over and over again
+function draw() {
+  if (frameCount % 20===0){
+    background (255,255,255);
+  } else {
+    background (0,0,0);
+
+  }
+ //an RGB color for the canvas' background
+  //circle
+  
+  
+  stroke(62,0)
+  fill(255,255,255);
+  textSize(90);
+  textFont('Papyrus');
+  text('Escape',100,290);
+  stroke(154,210)
+  fill(255,0,0);
+  textSize(30);
+  textFont('Papyrus');
+  text('Fear',100,150);
+  stroke(154,210)
+  fill(255,255,255);
+  textSize(40);
+  textFont('Papyrus');
+  text('Unknown',330,400);
+  stroke(154,210)
+  fill(255,255,255);
+  textSize(64);
+  textFont('Papyrus');
+  text('Run',46,343);
+  stroke(154,210)
+  fill(255,0,0);
+  textSize(90);
+  textFont('Papyrus');
+  text('Danger',10,210);
+
+  stroke(154,210)
+  fill(255,255,255);
+  textSize(20);
+  textFont('Papyrus');
+  text('Escape',330,100);
+  stroke(62,0)
+  fill(255,0,0);
+  textSize(76);
+  textFont('Papyrus');
+  text('Fear',339,350);
+  stroke(154,210)
+  fill(255,0,0);
+  textSize(100);
+  textFont('Papyrus');
+  text('Unknown',100,70);
+  stroke(154,210)
+  fill(255,255,255);
+  textSize(100);
+  textFont('Papyrus');
+  text('Run',330,180);
+  stroke(62,0)
+  fill(255,0,0);
+  textSize(36);
+  textFont('Papyrus');
+  text('Danger',220,500);
+
+  stroke(154,210)
+  fill(250,0,0);
+  textSize(48);
+  textFont('Papyrus');
+  text('Escape',350,490);
+  stroke(62,0)
+  fill(255,0,0);
+  textSize(100);
+  textFont('Papyrus');
+  text('Fear',11,500);
+  stroke(154,210)
+  fill(255,255,255);
+  textSize(75);
+  textFont('Papyrus');
+  text('Unknown',0,432);
+  stroke(154,210)
+  fill(255,255,255);
+  textSize(40);
+  textFont('Papyrus');
+  text('Run',410,450);
+  stroke(62,210)
+  fill(255,0,0);
+  textSize(25);
+  textFont('Papyrus');
+  text('Danger',9,60);
+
+  stroke(154,210)
+  fill(255,255,255);
+  textSize(48);
+  textFont('Papyrus');
+  text('Escape',-20,110);
+  stroke(154,210)
+  fill(255,255,255);
+  textSize(50);
+  textFont('Papyrus');
+  text('Fear',430,120);
+  stroke(154,210)
+  fill(255,255,255);
+  textSize(50);
+  textFont('Papyrus');
+  text('Unknown',400,240);
+  stroke(154,210)
+  fill(255,255,255);
+  textSize(40);
+  textFont('Papyrus');
+  text('Run',200,130);
+  stroke(154,210)
+  fill(255,0,0);
+  textSize(45);
+  textFont('Papyrus');
+  text('Danger',150,380);
+
+ 
+  
+
+}
+
+
+
+
+
+
